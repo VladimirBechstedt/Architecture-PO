35e11a2d113c228dd3aa61ab9c3a2fa8716c5be9
vladimir <wowa1998bwp@gmail.com>
2023-11-10 22:09:02 +0600

    sexth

---
+++ lesson06/task.sql
@@ -0,0 +1,20 @@
+ALTER TABLE `healthHistory` DROP FOREIGN KEY `healthHistory_fk0`;
+
+ALTER TABLE `healthHistory` DROP FOREIGN KEY `healthHistory_fk1`;
+
+ALTER TABLE `healthHistory` DROP FOREIGN KEY `healthHistory_fk2`;
+
+ALTER TABLE `Patient` DROP FOREIGN KEY `Patient_fk0`;
+
+ALTER TABLE `schedule` DROP FOREIGN KEY `schedule_fk0`;
+
+ALTER TABLE `schedule` DROP FOREIGN KEY `schedule_fk1`;
+
+ALTER TABLE `Doctor` DROP FOREIGN KEY `Doctor_fk0`;
+
+ALTER TABLE `Doctor` DROP FOREIGN KEY `Doctor_fk1`;
+
+ALTER TABLE `vaccinationCard` DROP FOREIGN KEY `vaccinationCard_fk0`;
+
+ALTER TABLE `vaccinationCard` DROP FOREIGN KEY `vaccinationCard_fk1`;
+
